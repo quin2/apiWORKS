59ddf4e94e56e0a57845e474478143d4bd497cba
Quinn Vinlove <quinnvinlove@skylab.org>
2022-07-03 20:48:38 -0500

    Initial Commit

---
+++ apiWORKS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apiWORKS
+//
+//  Created by Quinn Vinlove on 7/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apiWORKS/apiWORKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiWORKSApp.swift
+//  apiWORKS
+//
+//  Created by Quinn Vinlove on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct apiWORKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
